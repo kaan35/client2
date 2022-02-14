f6b6e3c1745c48ff5157b1e7823b78ddf6c32d6e
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-02-14 15:47:54 +0300

    Added index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { IndexComponent } from './pages/index/index.component'
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +19,4 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
