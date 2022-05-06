5764ab13d4ebbb7ecfe41e3fbd83987e63823409
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-05-06 15:04:36 +0300

    Updated navbar with mobile menu support

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
-  constructor () { }
+  showNavbarModal: boolean = false;
 
-  ngOnInit (): void { }
+  toggleNavBarModal() {
+    this.showNavbarModal = !this.showNavbarModal;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
